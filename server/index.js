4324eb93573de22a4f6d98e213944a96bffc0373
afifasoft <abdul.pesit@gmail.com>
2020-04-24 14:55:22 +0530

    changed greeting

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-// 11
+// 16
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
