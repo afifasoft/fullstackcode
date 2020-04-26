d4915dcc8d23a5092ecc67ef21a317a3ba647e2d
afifasoft <abdul.pesit@gmail.com>
2020-04-26 16:07:10 +0530

    Securing API keys

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-// 16
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
