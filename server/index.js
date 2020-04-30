30153d7dda6ccb3338dcc6d4e976ee40daba6106
afifasoft <abdul.pesit@gmail.com>
2020-04-30 07:19:40 +0530

    Signing In Users with OAuth

--- server/index.js
+++ server/index.js
@@ -1,17 +1,10 @@
 const express = require("express");
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
+require('./services/passport');
 const app = express();
-// 25
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-  console.log(accessToken);
-}));
+
+//const authRoutes = require('./routes/authRoutes');
+//authRoutes(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
