a27bfc07898d49a57b0755058475c46e0afde795
afifasoft <abdul.pesit@gmail.com>
2020-05-03 07:25:24 +0530

    Connecting Mongoose to Mongo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+// 35
 //const authRoutes = require('./routes/authRoutes');
 //authRoutes(app);
 require('./routes/authRoutes')(app);
