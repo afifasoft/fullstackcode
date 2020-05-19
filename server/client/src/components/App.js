e7b671eb6271c21b2719daf6569aa12dbc748c5d
afifasoft <abdul.pesit@gmail.com>
2020-05-19 07:47:02 +0530

    Route Configuration

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+const Header = () => {
+  return (
+    <h2>Header</h2>
+  );
+}
+
+const Dashboard = () => {
+  return (
+    <h2>Dashboard</h2>
+  )
+}
+
+const SurveyNew = () => {
+  return (
+    <h2>SurveyNew</h2>
+  )
+}
+
+const Landing = () => {
+  return (
+    <h2>Landing</h2>
+  )
+}
 
 const App = () =>  {
     return (
       <div>
-        Hi There!
+        <BrowserRouter>
+          <div>
+            <Route path="/" component={Landing}/>
+          </div>
+        </BrowserRouter>
       </div>
     );
 };
