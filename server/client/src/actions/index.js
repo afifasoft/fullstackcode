bfa47a353e0b0b023fc792a141091deb288815b6
afifasoft <abdul.pesit@gmail.com>
2020-05-21 06:31:37 +0530

    Refactoring to Async/Await

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,18 +1,13 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
+export const fetchUser = () => async (dispatch) => {
+     //  const response = axios.get('/api/current_user');
+     // return {
+     //   type: FETCH_USER,
+     //   payload: response
+     // }
+    const res = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res.data });
 
-  //  const response = axios.get('/api/current_user');
-
-  // return {
-  //   type: FETCH_USER,
-  //   payload: response
-  // }
-  return function(dispatch) {
-    axios.get('/api/current_user')
-      .then(res => {
-        dispatch({ type: FETCH_USER, payload: res.data });
-      })
-  }
-};
+}
