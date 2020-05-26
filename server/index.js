02d034fd9a57c3ad869740d4d88dcbdbe8987532
afifasoft <abdul.pesit@gmail.com>
2020-05-26 07:55:42 +0530

    BodyParser Middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json())
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,7 +23,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// 106
+// 110
 //const authRoutes = require('./routes/authRoutes');
 //authRoutes(app);
 require('./routes/authRoutes')(app);
