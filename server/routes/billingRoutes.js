dbd24005c08c505f65b1267f556c5d891e0f83d1
afifasoft <abdul.pesit@gmail.com>
2020-05-29 09:24:32 +0530

    Creating a charge object

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,7 +1,17 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
+  app.post('/api/stripe', async (req, res) => {
 
+    const charge = await stripe.charges.create({
+        amount: 500,
+        currency: 'inr',
+        description: 'Rs.500 for 5 credits',
+        source: req.body.id
+
+      });
+
+      console.log(charge);
   });
+
 };
