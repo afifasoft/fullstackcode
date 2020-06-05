5149bfe32ea1fe3a1dcda5a591ecf6d4d8048a1e
afifasoft <abdul.pesit@gmail.com>
2020-06-05 08:38:06 +0530

    Added billing and client side app!

--- server/index.js
+++ server/index.js
@@ -23,11 +23,24 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// 115
+// 120
 //const authRoutes = require('./routes/authRoutes');
 //authRoutes(app);
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV == 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
