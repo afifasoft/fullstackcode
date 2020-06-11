c886a15facd0bb33d2f28a5f92c745b90f5665d5
afifasoft <abdul.pesit@gmail.com>
2020-06-11 08:09:26 +0530

    Creating Surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-    
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title: title,
+      subject: subject,
+      body: body
+
+    });
   })
 };
