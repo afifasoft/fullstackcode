552715ff3688c4a2cb54470243699ac8ed2f9c53
afifasoft <abdul.pesit@gmail.com>
2020-06-16 08:14:21 +0530

    Boilerplate for sending Emails

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -8,10 +8,33 @@ class Mailer extends helper.Mail {
     this.from = new helper.Email('abdul.pesit@gmail.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
+    // Helper function
     this.recipients = this.formatAddresses(recipients);
 
+    // Mailer Built in function
+    this.addContent(this.body);
+    // Helper function
+    this.addClickTracking();
+    this.addRecipients();
 
   }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    // Mailer Built in functions
+    this.addTrackingSettings(trackingSettings);
+  }
+
+
 }
 
 module.exports = Mailer;
