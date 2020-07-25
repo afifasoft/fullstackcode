5aca07fbedf23125b2cc5699b7861f852b77a81c
afifasoft <abdul.pesit@gmail.com>
2020-07-25 09:06:39 +0530

    Displaying Invalid email

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,18 @@
+// SurveyField contains logic to render a single label and text input
+import React from 'react';
+
+export default ({ input, label, meta: { error, touched } }) => {
+
+  return (
+    <div className="row">
+      <div className="col s6">
+        <label>{label}</label>
+          <input {...input} style={{ marginBottom : '5px' }} />
+            <div className="red-text" style={{ marginBottom: '20px' }}>
+              {touched && error}
+            </div>
+        </div>
+
+    </div>
+  );
+};
---
+++ server/client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,66 @@
+// SurveyForm shows a form for a user to add input
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title', noValueError: 'Provide a  Survey Title' },
+  { label: 'Subject Line', name: 'subject',  noValueError: 'Provide a Subject'},
+  { label: 'Email Body', name: 'body', noValueError: 'Provide a  body' },
+  { label: 'Recipient List', name: 'emails', noValueError: 'Provide a  email' }
+];
+
+class SurveyForm extends Component {
+
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log('values: ',values))}>
+          {this.renderFields()}
+
+          <div className="row">
+            <div className="col s6">
+              <Link to="/surveys" className="red btn-flat white-text">
+                Cancel
+              </Link>
+              <button type="submit" className="teal btn-flat right white-text">
+                <i className="material-icons right">done</i>
+                Next</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  })
+
+
+  return errors;
+
+}
+
+export default reduxForm({
+  validate: validate,
+  form: 'surveyForm'
+})(SurveyForm);
