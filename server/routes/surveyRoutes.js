9ddda64d5e8e48a380c5c91173d1b21223d6a9a0
afifasoft <abdul.pesit@gmail.com>
2020-10-04 09:13:07 +0530

    Finding the Exact Survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -16,24 +16,24 @@ module.exports = (app) => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      console.log(p.test(pathname));
-      const  match = p.test(pathname);
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
-      if (match) {
-        return {
-          email,
-          surveyId: match.surveyId,
-          choice: match.choice
-        };
-      }
-    });
-    console.log(events);
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-    console.log(uniqueEvents);
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+          const  match = p.test(new URL(url).pathname);
+          if (match) {
+            return {
+              email,
+              surveyId: match.surveyId,
+              choice: match.choice
+            };
+          }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+
+      console.log(events);
     res.send({});
   });
 
